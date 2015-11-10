ae7f57b85b8490bba91d3325e77f78d166dca48d
ritaranjit <rita.ranjit23@gmail.com>
2015-11-10 12:42:17 -0500

    simple interest added

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SImpleInterest;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetSI()
+        {
+            SimpleInterest tt = new SimpleInterest();
+            int output = tt.getSI(100,12,3);
+            Assert.AreEqual(36,output);
+        }
+    }
+}
