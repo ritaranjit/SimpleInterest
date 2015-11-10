02228a41d6badc5cf6297a5d92f7742aa053d1d9
ritaranjit <rita.ranjit23@gmail.com>
2015-11-10 12:40:01 -0500

    Added Simple Interest

---
+++ SImpleInterest/SimpleInterest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SImpleInterest
+{
+    public class SimpleInterest
+    {
+        public int getSI(int p, int t, int r)
+        {
+            return (p * t * r) / 100;
+        }
+    }
+}
